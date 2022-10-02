a0970a3fe26af3b3d712058a85e366e94de1e222
fra <fra.sette.p.c@gmail.com>
2022-10-02 21:27:46 +0200

    stuff

--- APP/app/src/main/java/com/example/app/MainActivity.java
+++ APP/app/src/main/java/com/example/app/MainActivity.java
@@ -2,15 +2,29 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    Button b1 = findViewById(R.id.button1);
 
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.label1);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 }
